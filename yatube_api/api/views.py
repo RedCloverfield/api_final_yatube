3b37b9a0aceda3edde56cbe0e3a19c47fdc2438e
Станислав Ефимов <stanislav.karov@mail.ru>
2024-02-12 00:01:06 +0300

    Create viewsets for project models

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,1 +1,44 @@
-# TODO:  Напишите свой вариант
+from django.shortcuts import get_object_or_404
+from rest_framework import viewsets
+
+from posts.models import Group, Post
+from .serializers import CommentSerializer, FollowSerializer, GroupSerializer, PostSerializer
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    serializer_class = CommentSerializer
+
+    def get_post_object(self):
+        return get_object_or_404(Post, pk=self.kwargs['post_id'])
+
+    def get_queryset(self):
+        return get_object_or_404().comments.all()
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user, post=self.get_post_object())
+
+
+class GroupViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+
+
+class FollowViewSet(viewsets.ModelViewSet):
+    serializer_class = FollowSerializer
+
+    def get_object(self):
+        return self.request.user
+
+    def get_queryset(self):
+        return self.get_object().followed_users.all()
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.get_object())
