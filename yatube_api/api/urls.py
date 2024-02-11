87d24e2b1a14f7126dd95c6e68192ccc3f39f15c
Станислав Ефимов <stanislav.karov@mail.ru>
2024-02-12 00:01:36 +0300

    Create API endpoints

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -1,4 +1,17 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
+from .views import PostViewSet, GroupViewSet, CommentViewSet, FollowViewSet
+
+router_v1 = DefaultRouter()
+
+router_v1.register('posts', PostViewSet, basename='post')
+router_v1.register('groups', GroupViewSet, basename='group')
+router_v1.register(r'posts/(?P<post_id>[/d]+)/comments', CommentViewSet, basename='comment')
+router_v1.register('follow', FollowViewSet, basename='follow')
+
 
 urlpatterns = [
+    path('auth/', include('djoser.urls.jwt')),
+    path('v1/', include(router_v1.urls)),
 ]
