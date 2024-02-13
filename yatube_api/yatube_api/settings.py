0055d75fe9d80db7f214b9bc725deb6e85a736fa
Станислав Ефимов <stanislav.karov@mail.ru>
2024-02-13 20:43:38 +0300

    IMPORTANT FIX. Correct key name of REST_FRAMEWORK dict

--- yatube_api/yatube_api/settings.py
+++ yatube_api/yatube_api/settings.py
@@ -96,7 +96,7 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.IsAuthenticated',
     ],
 
-    'DEFAUL_AUTHENTICATION_CLASSES': [
+    'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
 }
