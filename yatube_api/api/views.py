395f79d472d65561b61eae2547dd5836ed8dc615
Станислав Ефимов <stanislav.karov@mail.ru>
2024-02-14 21:37:16 +0300

    Configure permissions for viewsets

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,14 +1,17 @@
 from django.shortcuts import get_object_or_404
 from rest_framework import filters, mixins, viewsets
 from rest_framework.pagination import LimitOffsetPagination
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 from posts.models import Group, Post
+from .permissions import IsAuthorizedOrReadOnly
 from .serializers import CommentSerializer, FollowSerializer, GroupSerializer, PostSerializer
 
 
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+    permission_classes = (IsAuthenticatedOrReadOnly&IsAuthorizedOrReadOnly,)
     pagination_class = LimitOffsetPagination
 
     def perform_create(self, serializer):
@@ -17,6 +20,7 @@ class PostViewSet(viewsets.ModelViewSet):
 
 class CommentViewSet(viewsets.ModelViewSet):
     serializer_class = CommentSerializer
+    permission_classes = (IsAuthenticatedOrReadOnly&IsAuthorizedOrReadOnly,)
 
     def get_post_object(self):
         return get_object_or_404(Post, pk=self.kwargs['post_id'])
@@ -31,6 +35,7 @@ class CommentViewSet(viewsets.ModelViewSet):
 class GroupViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Group.objects.all()
     serializer_class = GroupSerializer
+    permission_classes = (IsAuthenticatedOrReadOnly,)
 
 
 class FollowViewSet(mixins.CreateModelMixin, mixins.ListModelMixin, viewsets.GenericViewSet):
