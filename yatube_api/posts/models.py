2eff7e1b357e6352007d857e19375b09c20a0aa4
Станислав Ефимов <stanislav.karov@mail.ru>
2024-02-15 20:43:17 +0300

    Add default ordering to Post model

--- yatube_api/posts/models.py
+++ yatube_api/posts/models.py
@@ -19,6 +19,9 @@ class Post(models.Model):
     def __str__(self):
         return self.text[:50]
 
+    class Meta:
+        ordering = ('pub_date',)
+
 
 class Comment(models.Model):
     author = models.ForeignKey(
